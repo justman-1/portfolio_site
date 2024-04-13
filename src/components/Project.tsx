985f3015a9cc5d48e05e5de0fd029697e9f85964
justman-1 <sharom573@gmail.com>
2024-04-13 14:41:40 +0300

    1

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,25 +9,67 @@ interface propsType {
   using: boolean
 }
 
+let loaded: boolean = false
+let imgChanging: boolean = false
 let changeImgI: number = 0
+let imgWid: number = 1;
+let imgSourcesCopy: string[] = []
+let imgLast = -1
 
 export default function Project(props: propsType): JSX.Element {
   const [imgI, setImgI] = useState<number>(0)
   const line = useRef<HTMLDivElement>(null)
+  const imgsRef = useRef<HTMLDivElement>(null)
+  const [imgSources, setImgSources] = useState<string[]>([])
+  const [imgTrans, setImgTrans] = useState<number>(1)
+  useEffect(()=>{
+    if(!loaded){
+      loaded = true;
+      imgWid = imgsRef.current!.offsetWidth;
+      console.log(imgWid)
+      console.log("props: ")
+      console.log(props)
+      let empty: string[] = []
+      let preSources: string[] = props.imgSources
+      preSources.unshift(preSources[preSources.length-1])
+      preSources.push(preSources[1])
+      console.log(props.imgSources)
+      console.log(preSources)
+      setImgSources(empty.concat(preSources))
+      imgSourcesCopy = preSources
+      imgLast = 0
+    }
+  }, [])
   useEffect(() => {
     console.log("using: " + props.using)
-    if (props.using) {
+    if (props.using && !imgChanging) {
+      imgChanging = true
       line.current!.style.transition = "all 7s linear"
       line.current!.style.width = "95%"
       let saveChangeImgI: number = changeImgI
       setTimeout(() => {
         if (saveChangeImgI == changeImgI) {
           changeImgI++
+          imgLast = imgLast == props.imgSources.length-1 ? 0 : imgLast+1
           line.current!.style.transition = "all 0s linear"
           line.current!.style.width = "0%"
+          let preSources: string[] = imgSourcesCopy
+          let empty: string[] = []
+          preSources.push(props.imgSources[imgLast])
+          preSources.splice(0, 1)
+          imgSourcesCopy = preSources
+          console.log(imgSources)
+          console.log(preSources)
+          setImgTrans(0)
+          setImgSources(empty.concat(preSources))
+          setImgI(imgI-1)
           setTimeout(
-            () => setImgI(imgI != props.imgSources.length - 1 ? imgI + 1 : 0),
-            10
+            () => {
+              imgChanging = false
+              setImgTrans(1)
+              setImgI(prev => prev != props.imgSources.length - 3 ? prev + 1 : 0)
+            },
+            50
           )
         }
       }, 7000)
@@ -40,14 +82,14 @@ export default function Project(props: propsType): JSX.Element {
         <div className={st.projectTextDesc}>{props.descText}</div>
       </div>
       <div className={st.projectImages}>
-        <div className={st.projectImgsFlex}>
-          {props.imgSources.map((e, i) => {
+        <div className={st.projectImgsFlex} ref={imgsRef}>
+          {imgSources.map((e, i) => {
             return (
               <Image
                 className={st.projectImg}
                 loading="lazy"
                 src={`/portfolio${e}`}
-                style={{ display: imgI == i ? "block" : "none" }}
+                style={{marginLeft: i == 0 ? -(imgI+1) * imgWid + "px" : "0px", transition: `all ${imgTrans}s ease`}}
                 key={i}
                 width={900}
                 height={900}
