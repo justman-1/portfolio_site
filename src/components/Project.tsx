6d61977ccde11339ef66c1053393347950d8d977
justman-1 <sharom573@gmail.com>
2026-05-03 20:39:00 +0300

    proj bug

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,88 +1,88 @@
-import Image from "next/image"
-import { useEffect, useRef, useState } from "react"
-import st from "../styles/Projects.module.scss"
-import { useRouter } from "next/router"
-import { useAppDispatch } from "@/store/hook"
-import { portfolioAppear } from "@/store/loadSlice"
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import st from "../styles/Projects.module.scss";
+import { useRouter } from "next/router";
+import { useAppDispatch } from "@/store/hook";
+import { portfolioAppear } from "@/store/loadSlice";
 
 interface propsType {
-  headText: string
-  descText: string
-  imgSources: string[]
-  using: boolean
+  headText: string;
+  descText: string;
+  imgSources: string[];
+  using: boolean;
 }
 
-export default function Project(props: propsType): JSX.Element {
-  const dispatch = useAppDispatch()
-  const router = useRouter()
-  const line = useRef<HTMLDivElement>(null)
-  const imgsRef = useRef<HTMLDivElement>(null)
-  const loaded = useRef<boolean>(false)
-  const changeImgI = useRef<number>(0)
-  const isImgChanging = useRef<boolean>(false)
-  const changedByClick = useRef<boolean>(false)
-  const [imgCurr, setImgCurr] = useState<number>(-1)
-  const [imgSources, setImgSources] = useState<string[]>([])
+export default function Project(props: propsType) {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const line = useRef<HTMLDivElement>(null);
+  const imgsRef = useRef<HTMLDivElement>(null);
+  const loaded = useRef<boolean>(false);
+  const changeImgI = useRef<number>(0);
+  const isImgChanging = useRef<boolean>(false);
+  const changedByClick = useRef<boolean>(false);
+  const [imgCurr, setImgCurr] = useState<number>(-1);
+  const [imgSources, setImgSources] = useState<string[]>([]);
 
   function changeImgEvent(isRight: boolean) {
-    changeImg(isRight, true)
-    lineAnimation(false)
-    changedByClick.current = true
+    changeImg(isRight, true);
+    lineAnimation(false);
+    changedByClick.current = true;
   }
   function lineAnimation(on: boolean) {
-    if (!line.current) return
-    line.current.style.transition = on ? "all 7s linear" : "all 0s linear"
-    line.current.style.width = on ? "95%" : "0%"
+    if (!line.current) return;
+    line.current.style.transition = on ? "all 7s linear" : "all 0s linear";
+    line.current.style.width = on ? "95%" : "0%";
   }
   function changeImg(toRight: boolean, isButton: boolean) {
-    if (isImgChanging.current || !imgsRef.current) return
-    changeImgI.current++
-    isImgChanging.current = true
-    imgsRef.current.style.transition = `all ${isButton ? 0.5 : 1}s ease`
-    setTimeout(() => (isImgChanging.current = false), isButton ? 100 : 800)
-    if (toRight) return setImgCurr((prev) => (prev + 1) % imgSources.length)
-    setImgCurr((prev) => (imgSources.length + prev - 1) % imgSources.length)
+    if (isImgChanging.current || !imgsRef.current) return;
+    changeImgI.current++;
+    isImgChanging.current = true;
+    imgsRef.current.style.transition = `all ${isButton ? 0.5 : 1}s ease`;
+    setTimeout(() => (isImgChanging.current = false), isButton ? 100 : 800);
+    if (toRight) return setImgCurr((prev) => (prev + 1) % imgSources.length);
+    setImgCurr((prev) => (imgSources.length + prev - 1) % imgSources.length);
   }
   function goToPortfolioPage() {
-    dispatch(portfolioAppear(true))
+    dispatch(portfolioAppear(true));
   }
 
   useEffect(() => {
     if (!loaded.current) {
-      loaded.current = true
-      let preSources: string[] = props.imgSources
-      setImgSources(preSources)
-      setImgCurr(0)
+      loaded.current = true;
+      let preSources: string[] = props.imgSources;
+      setImgSources(preSources);
+      setImgCurr(0);
     }
-  }, [])
+  }, []);
   useEffect(() => {
     if (!props.using) {
-      if (imgCurr != 0) setImgCurr(0)
-      return
+      if (imgCurr != 0) setImgCurr(0);
+      return;
     }
     if (changedByClick.current) {
-      changedByClick.current = false
-      let saveImgChangeI: number = changeImgI.current
+      changedByClick.current = false;
+      let saveImgChangeI: number = changeImgI.current;
       setTimeout(() => {
         if (saveImgChangeI == changeImgI.current)
-          setImgCurr((prev) => (prev + 1) % imgSources.length)
-      }, 10000)
-      return
+          setImgCurr((prev) => (prev + 1) % imgSources.length);
+      }, 10000);
+      return;
     }
     if (imgSources.length) {
-      lineAnimation(false)
+      lineAnimation(false);
       setTimeout(() => {
-        lineAnimation(true)
-        changeImgI.current++
-        let saveImgChangeI: number = changeImgI.current
+        lineAnimation(true);
+        changeImgI.current++;
+        let saveImgChangeI: number = changeImgI.current;
         setTimeout(async () => {
           if (saveImgChangeI == changeImgI.current) {
-            setTimeout(() => changeImg(true, false), 30)
+            setTimeout(() => changeImg(true, false), 30);
           }
-        }, 7000)
-      }, 50)
+        }, 7000);
+      }, 50);
     }
-  }, [imgCurr, props.using])
+  }, [imgCurr, props.using]);
   return (
     <>
       <div
@@ -126,7 +126,7 @@ export default function Project(props: propsType): JSX.Element {
                         priority
                       />
                     </div>
-                  )
+                  );
                 })}
               </div>
               <div className={st.photoLine} ref={line}></div>
@@ -170,5 +170,5 @@ export default function Project(props: propsType): JSX.Element {
         )}
       </div>
     </>
-  )
+  );
 }
